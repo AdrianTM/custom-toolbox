5fb2bac410d33ae302a193eb96103fd7a93912a1
Adrian <adrian@mxlinux.org>
2017-02-10 16:11:53 -0500

    new build
    Former-commit-id: 464331b66c3e3fb1f4dee91566360903404ace77

--- translations/custom-toolbox_sv.ts
+++ translations/custom-toolbox_sv.ts
@@ -56,17 +56,17 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="262"/>
+        <location filename="../mainwindow.ui" line="264"/>
         <source>This is a custom launcher</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="278"/>
+        <location filename="../mainwindow.ui" line="280"/>
         <source>Show this dialog at start up</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="300"/>
+        <location filename="../mainwindow.ui" line="302"/>
         <source>search</source>
         <translation type="unfinished"></translation>
     </message>
