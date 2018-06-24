0f53b2834f06cc351cace907321698eed3cf3dfc
Adrian <adrian@mxlinux.org>
2018-06-24 09:22:25 -0400

    add changelog, increased icon size, focus on search

---
+++ custom-toolbox_0.18.6.1.dsc
@@ -0,0 +1,32 @@
+-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA256
+
+Format: 3.0 (native)
+Source: custom-toolbox
+Binary: custom-toolbox
+Architecture: any
+Version: 0.18.6.1
+Maintainer: Adrian <adrian@mxlinux.org>
+Standards-Version: 3.9.8
+Vcs-Git: git://github.com/AdrianTM/custom-toolbox
+Build-Depends: qt5-qmake, qt5-default, qttools5-dev-tools, debhelper (>= 9)
+Package-List:
+ custom-toolbox deb admin optional arch=any
+Checksums-Sha1:
+ 3b7d9aaee69172c2b60e704e3966430627c0839e 26540 custom-toolbox_0.18.6.1.tar.xz
+Checksums-Sha256:
+ 101b8db9360b681d8bdcb39711f13538c15b177ae6b4b32239acc86e5348a68f 26540 custom-toolbox_0.18.6.1.tar.xz
+Files:
+ 7f40279b8280ae9b8caf4e2c33075e73 26540 custom-toolbox_0.18.6.1.tar.xz
+
+-----BEGIN PGP SIGNATURE-----
+
+iQFHBAEBCAAxFiEE8ndToY6S45N+YzXncJOMeAZ57pgFAlsvljQTHGFkcmlhbkBt
+eGxpbnV4Lm9yZwAKCRBwk4x4BnnumBX+CADBtCmyZ4rT2kAj/htzMMPKMgEbp7h4
+unIYu3OVStdTrQUa/vq8Gt8Nl0OGT0fJ8QqD++KaYfRF+qYLSq46csnyd20lOTr2
+HCl/Z9if2Nb5W2ztzi6GFqoDco15AE3U7Nnqq8Fcb1jOuBV60mUDjXcHYAZ6Gycs
+5a4cdXUg9PcuiV2RvlRbwV+EjQ2lj7UWsELDiWHXUXVDQnX+upb9eSU8Xtr68oI+
+v+2wd6HD8gRYH4/YO7T2H6QZCFVAhjFR0yTNLCXoL64Zs30jC8i2WN/Udjz/8XW0
+4EzOrf5H3on692sPUQW9lYcb7xf9X90EIhAIu4jg5IsmZwEqsMlB+Cpt
+=Oj5J
+-----END PGP SIGNATURE-----
--- translations/custom-toolbox_el.ts
+++ translations/custom-toolbox_el.ts
@@ -5,8 +5,8 @@
     <name>MainWindow</name>
     <message>
         <location filename="../mainwindow.ui" line="20"/>
-        <location filename="../mainwindow.cpp" line="111"/>
-        <location filename="../mainwindow.cpp" line="392"/>
+        <location filename="../mainwindow.cpp" line="112"/>
+        <location filename="../mainwindow.cpp" line="396"/>
         <source>Custom Toolbox</source>
         <translation type="unfinished"></translation>
     </message>
@@ -71,79 +71,89 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="180"/>
+        <location filename="../mainwindow.cpp" line="182"/>
         <source>Open List File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="180"/>
+        <location filename="../mainwindow.cpp" line="182"/>
         <source>List Files (*.list)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="182"/>
-        <location filename="../mainwindow.cpp" line="365"/>
-        <location filename="../mainwindow.cpp" line="446"/>
+        <location filename="../mainwindow.cpp" line="184"/>
+        <location filename="../mainwindow.cpp" line="369"/>
+        <location filename="../mainwindow.cpp" line="472"/>
         <source>File Open Error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="182"/>
+        <location filename="../mainwindow.cpp" line="184"/>
         <source>Could not open file, do you want to try again?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="365"/>
+        <location filename="../mainwindow.cpp" line="369"/>
         <source>Could not open file: </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="365"/>
+        <location filename="../mainwindow.cpp" line="369"/>
         <source>Application will close.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="391"/>
+        <location filename="../mainwindow.cpp" line="395"/>
         <source>About Custom Toolbox</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="392"/>
+        <location filename="../mainwindow.cpp" line="396"/>
         <source>Version: </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="393"/>
+        <location filename="../mainwindow.cpp" line="397"/>
         <source>Custom Toolbox is a tool used for creating a custom launcher</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="395"/>
+        <location filename="../mainwindow.cpp" line="399"/>
         <source>Copyright (c) MX Linux</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="397"/>
+        <location filename="../mainwindow.cpp" line="401"/>
+        <source>Changelog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="402"/>
         <source>Cancel</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="396"/>
+        <location filename="../mainwindow.cpp" line="400"/>
         <source>License</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="446"/>
+        <location filename="../mainwindow.cpp" line="417"/>
+        <source>&amp;Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="472"/>
         <source>Could not write file: </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="472"/>
+        <location filename="../mainwindow.cpp" line="498"/>
         <source>Error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="472"/>
+        <location filename="../mainwindow.cpp" line="498"/>
         <source>Could not find the &apos;%1&apos; editor. Please change the default editor in &apos;/etc/custom-toolbox/custom-toolbox.conf&apos; file.</source>
         <translation type="unfinished"></translation>
     </message>
