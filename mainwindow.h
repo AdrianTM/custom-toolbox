a650973dccb9549bfe3e2b12d3f95d44738c5351
Adrian <adrian@mxlinux.org>
2020-01-06 17:09:41 -0500

    fix wording
    Former-commit-id: 971b5592e5d3337ebcbef88dc9efb810d4e1177f

--- mainwindow.h
+++ mainwindow.h
@@ -43,7 +43,7 @@ class MainWindow : public QDialog
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
     QIcon findIcon(QString icon_name);
