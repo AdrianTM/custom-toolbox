74e32056c6a7efa79c17b9299736e7c52a3e24ac
Adrian <adrian@mxlinux.org>
2021-01-24 16:14:25 -0500

    use at() to access elements; improve editor detection
    Former-commit-id: 15338f8ff62769938863bd564a0f5b14c9b493b2

--- cmd.cpp
+++ cmd.cpp
@@ -20,19 +20,19 @@ void Cmd::halt()
 
 bool Cmd::run(const QString &cmd, bool quiet)
 {
-    QByteArray output;
+    QString output;
     return run(cmd, output, quiet);
 }
 
 // util function for getting bash command output
 QString Cmd::getCmdOut(const QString &cmd, bool quiet)
 {
-    QByteArray output;
+    QString output;
     run(cmd, output, quiet);
     return output;
 }
 
-bool Cmd::run(const QString &cmd, QByteArray &output, bool quiet)
+bool Cmd::run(const QString &cmd, QString &output, bool quiet)
 {
     if (this->state() != QProcess::NotRunning) {
         qDebug() << "Process already running:" << this->program() << this->arguments();
