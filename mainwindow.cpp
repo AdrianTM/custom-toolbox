ec2a0e30f9dbd2c50358f2c30aa9772268909aa1
Adrian <adrian@mxlinux.org>
2023-01-21 09:51:02 -0500

    Generalize argument stripping

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -118,8 +118,8 @@ QIcon MainWindow::findIcon(QString icon_name)
     return QIcon(out);
 }
 
-// Strip %f|%F|%U if exec expects a file name since it's called without an argument from this launcher.
-void MainWindow::fixExecItem(QString &item) { item.remove(QRegularExpression(QStringLiteral(" %f| %F| %U"))); }
+// Strip %f, %F, %U, etc. if exec expects a file name since it's called without an argument from this launcher.
+void MainWindow::fixExecItem(QString &item) { item.remove(QRegularExpression(QStringLiteral(R"(%[a-zA-Z])"))); }
 
 void MainWindow::fixNameItem(QString &item)
 {
